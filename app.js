227613ebed686a97df30a1171dc5ab006c038fec
kartash.me <kartashovAlex2008@yandex.ru>
2026-01-06 18:10:43 +0300

    switching from ejs to nunjucks

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const express = require("express")
 const path = require("path")
 const http = require("http")
-const expressLayouts = require("express-ejs-layouts")
+const nunjucks = require("nunjucks")
 const main_router = require("./routes/main")
 const login_router = require("./routes/login")
 const wss = require("./src/websocket")
 
 const app = express()
 const server = http.createServer(app)
+const env = nunjucks.configure("views", {
+    autoescape: true,
+    trimBlocks: true,
+    lstripBlocks: true,
+    express: app,
+    watch: true,
+    noCache: true // only for dev
+})
 
-app.set("view engine", "ejs")
+app.set("view engine", "njk")
 app.set("views", path.join(__dirname, "views"))
-app.set("layout", "base")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(expressLayouts)
 app.use(express.static("static"))
 app.use(express.static("media"))
 app.use("/", main_router)
 app.use("/", login_router)
 
 wss.init_wss(server)
-server.listen(3000, '0.0.0.0', () => console.log("Listening on http://localhost:3000"))
+server.listen(3000, "0.0.0.0", () => console.log("Listening on http://localhost:3000"))
