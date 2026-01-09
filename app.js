5b3def1858653b9561a0554b6344ee8b53f3017f
kartash.me <kartashovAlex2008@yandex.ru>
2026-01-09 17:41:18 +0300

    new project architecture

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require("express")
 const path = require("path")
-const http = require("http")
+const express = require("express")
+
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
+
 const nunjucks = require("nunjucks")
+
 const main_router = require("./routes/main")
 const login_router = require("./routes/login")
 const register_router = require("./routes/register")
-const wss = require("./src/websocket")
 
 const app = express()
-const server = http.createServer(app)
 const env = nunjucks.configure("views", {
     autoescape: true,
     trimBlocks: true,
@@ -21,13 +23,17 @@ const env = nunjucks.configure("views", {
 app.set("view engine", "njk")
 app.set("views", path.join(__dirname, "views"))
 
+app.use(morgan("dev"))
+app.use(cookieParser())
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
 app.use(express.static("static"))
 app.use(express.static("media"))
+
 app.use("/", main_router)
 app.use("/", login_router)
 app.use("/", register_router)
 
-wss.init_wss(server)
-server.listen(3000, "0.0.0.0", () => console.log("Listening on http://localhost:3000"))
+module.exports = app
